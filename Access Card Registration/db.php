6da1997f9fd102ebeb76c29ae3edd17498bbec39
Aadit45 <58715615+Aadit45@users.noreply.github.com>
2022-08-15 13:14:48 +0545

    Update db.php

--- Access Card Registration/db.php
+++ Access Card Registration/db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","beast","12345","RFID_Access");
+    $con = mysqli_connect("localhost","beast","12345","door_lock");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
